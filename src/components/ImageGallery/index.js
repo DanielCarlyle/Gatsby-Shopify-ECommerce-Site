e4a00c432f9c3c417e248501315d1d09ebc85872
Daniel Carlyle <danielcarlyle@dan-carlyle-macbook-2.local>
2021-01-25 20:07:19 -0500

    latest commit

--- src/components/ImageGallery/index.js
+++ src/components/ImageGallery/index.js
@@ -3,16 +3,20 @@ import Image from 'gatsby-image';
 import { ImageGalleryWrapper } from './styles';
 import ImageThumbnail from './ImageThumbnail';
 
-export function ImageGallery( {images}) {
+export function ImageGallery({ selectedVariantImageId, images }) {
+  const [activeImageThumbnail, setActiveImageThumbnail] = React.useState(
+    images.find(({ id }) => id === selectedVariantImageId) || images[0]
+  );
+ 
+  React.useEffect(() => {
+    setActiveImageThumbnail(
+      images.find(({ id }) => id === selectedVariantImageId) || images[0]
+    );
+  }, [selectedVariantImageId, images, setActiveImageThumbnail]);
 
-//set States
-const [activeImageThumbnail, setActiveImageThumbnail] = React.useState(
- images[0]
-);
   const handleClick = image => {
-      setActiveImageThumbnail(image);
+    setActiveImageThumbnail(image);
   };
-
     return (
       <ImageGalleryWrapper>
       <div>
--- src/templates/ProductTemplate/index.js
+++ src/templates/ProductTemplate/index.js
@@ -1,12 +1,17 @@
+/* eslint-disable jsx-a11y/no-onchange */
 import React from 'react';
 import { graphql } from 'gatsby';
-import {Layout, ImageGallery} from 'components';
-import {Grid} from './styles';
+import {Layout, ImageGallery, ProductQuantityAdder} from 'components';
+import {Grid, SelectWrapper, Price} from './styles';
+import CartContext from 'context/CartContext';
+import { navigate, useLocation } from '@reach/router';
+import queryString from 'query-string';
 
 //Page Query - Product Component template is used for the pages
 export const query = graphql`
    query ProductQuery($shopifyId: String){
   shopifyProduct(shopifyId: {eq: $shopifyId }) {
+    shopifyId
     title
     description
     images {
@@ -23,17 +28,81 @@ export const query = graphql`
    }
 `;
 
-
 export default function ProductTemplate(props) {
+   //Destructure the GetProducts from
+   const { getProductById } = React.useContext(CartContext);
+   const [product, setProduct] = React.useState(null);
+   const [selectedVariant, setSelectedVariant] = React.useState(null);
+   const { search, origin, pathname } = useLocation();
+   //console.log(search, origin, pathname);
+   const variantId = queryString.parse(search).variant;
+
+   //Create a useEffect Hook
+   React.useEffect(() => {
+     getProductById(props.data.shopifyProduct.shopifyId).then(result => {
+       setProduct(result);
+       setSelectedVariant(
+          result.variants.find(({ id }) => id === variantId) ||
+       result.variants[0]
+     );
+     });
+   }, [getProductById,
+       setProduct,
+       props.data.shopifyProduct.shopifyId,
+       variantId,
+     ]);
+
+     const handleVariantChange = e => {
+       const newVariant = product?.variants.find(v => v.id === e.target.value);
+       setSelectedVariant(newVariant);
+       navigate(
+         `${origin}${pathname}?variant=${encodeURIComponent(newVariant.id)}`,
+         {
+           replace: true,
+         }
+       );
+     };
+
     //console.log(props);
     return <Layout>
            <Grid>
             <div>
             <h1>{props.data.shopifyProduct.title}</h1>
             <p>{props.data.shopifyProduct.description}</p>
+            {product?.availableForSale && !!selectedVariant && (
+            <>
+            {product?.variants.length > 1 && (
+            <SelectWrapper>
+              <strong>Variant</strong>
+              <select
+                value={selectedVariant.id}
+                onChange={handleVariantChange}
+              >
+                {product?.variants.map(v => (
+                  <option key={v.id} value={v.id}>
+                    {v.title}
+                  </option>
+                ))}
+              </select>
+            </SelectWrapper>
+          )}
+            {!!selectedVariant && (
+              <>
+                    <Price>${selectedVariant.price}</Price>
+                    <ProductQuantityAdder
+                      available={selectedVariant.available}
+                      variantId={selectedVariant.id}
+                    />
+                  </>
+                )}
+              </>
+          )}
             </div>
             <div>
-            <ImageGallery images={props.data.shopifyProduct.images} />
+              <ImageGallery
+                selectedVariantImageId={selectedVariant?.image.id}
+                images={props.data.shopifyProduct.images}
+              />
             </div>
             </Grid>
             </Layout>;
